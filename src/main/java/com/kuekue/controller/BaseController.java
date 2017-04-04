7289b0a1343d53447a1d03fff949e165f04d3ba4
kuekue <kuekue@qq.com>
2017-04-04 15:26:41 +0800

    新增模板样式

---
+++ src/main/java/com/kuekue/controller/BaseController.java
@@ -0,0 +1,16 @@
+package com.kuekue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BaseController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String index() {
+		return "Hello im lihao";
+	}
+
+}
